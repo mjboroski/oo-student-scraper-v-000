b5f212356738dd7352583e8f4f4dbaeeb24585da
Michael Boroski <mjboroski@gmail.com>
2018-05-03 04:48:24 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,12 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    doc=Nokogiri::HTML(open(index_url))
+    # doc=Nokogiri::HTML(open(index_url))
 
   end
 
   def self.scrape_profile_page(profile_url)
-    profile=Nokogiri::HTML(open(profile_url))
+    # profile=Nokogiri::HTML(open(profile_url))
 
   end
 
