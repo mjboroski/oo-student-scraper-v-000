935f2b97dc358c325882e447e0def75bef49297e
Michael Boroski <mjboroski@gmail.com>
2018-05-03 04:46:52 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     doc=Nokigiri::HTML(open(index_url))
-    
+
   end
 
   def self.scrape_profile_page(profile_url)
@@ -14,4 +14,3 @@ class Scraper
   end
 
 end
-
