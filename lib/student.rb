a73e7ef2bc59d3987425b2b2d7af3869159baedc
Michael Boroski <mjboroski@gmail.com>
2018-05-03 05:00:23 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,7 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.each{|student| Student.new(student)}
-    doc=Nokogiri::HTML
+    # doc=Nokogiri::HTML
   end
 
   def add_student_attributes(attributes_hash)
