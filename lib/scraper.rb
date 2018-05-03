5350a3f770d63f7a11f336a1e82d44547cae5470
Michael Boroski <mjboroski@gmail.com>
2018-05-03 05:00:33 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,15 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    # doc=Nokogiri::HTML(open(index_url))
+
+    doc=Nokogiri::HTML(open(index_url))
+
+
+    link=doc.search("student-card.div.student-name").text
+    name=doc.search("div.student-card.id").text
+    location=doc.search("div.student-card.id").text
+
+    student=Student.new(student_hash)
 
   end
 
